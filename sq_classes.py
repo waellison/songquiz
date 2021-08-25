dba0fd120b3abf1118d1f96c67d170bb74516ad5
William Ellison <waellison@me.com>
2021-08-25 13:44:14 -0400

    Moved game logic into a game class

--- sq_classes.py
+++ sq_classes.py
@@ -5,34 +5,54 @@ from tr import tr
 from functools import reduce
 
 
-def _make_match_candidate(string):
-    """Prepare a string for fuzzy matching.
+class sq_util:
+    @staticmethod
+    def make_match_candidate(string):
+        """
+        Prepare a string for fuzzy matching.
 
-    Removes punctuation, articles, and conjunctions to allow for some
-    measure of leniency in matching user input.
+        Removes punctuation, articles, and conjunctions to allow for some
+        measure of leniency in matching user input.
 
-    Argument:
-    string: the string to operate on
+        Argument:
+        string: the string to operate on
 
-    Returns: the match candidate string with punctuation, articles, and
-    conjunctions removed
-    """
-    punct = "!$&()-\"\';:,./?"
-    articles = ["a", "an", "the", "but", "and", "or"]
-    tmp = string
+        Returns: the match candidate string with punctuation, articles, and
+        conjunctions removed
+        """
+        punct = "!$&()-\"\';:,./?"
+        articles = ["a", "an", "the", "but", "and", "or"]
+        tmp = string
 
-    # Remove common punctuation from candidate strings
-    tmp = tr(punct, "", tmp, 'd')
+        # Remove common punctuation from candidate strings
+        tmp = tr(punct, "", tmp, 'd')
 
-    # Now remove articles and conjunctions from candidate strings
-    tmp_words = tmp.split()
-    for a_word in articles:
-        for s_word in tmp_words:
-            if s_word.lower() == a_word and len(tmp_words) > 1:
-                tmp_words.remove(s_word)
+        # Now remove articles and conjunctions from candidate strings
+        tmp_words = tmp.split()
+        for a_word in articles:
+            for s_word in tmp_words:
+                if s_word.lower() == a_word and len(tmp_words) > 1:
+                    tmp_words.remove(s_word)
 
-    # And return the result
-    return " ".join(tmp_words)
+        # And return the result
+        return " ".join(tmp_words)
+
+    feedbacks = {
+        "perfect":
+        ["Excellent!",
+         "Awesome!",
+         "Nailed it!",
+         "Ding-ding-ding!",
+         "You got it!"],
+        "title":
+        ["You got the song, but not the artist.",
+         "You knew the song, just not who sang it.",
+         "Nice, you got the title."],
+        "artist":
+        ["You got the artist, but not the title.",
+         "You knew the singer, just not the name of the song.",
+         "Nice, you got the artist."],
+    }
 
 
 class sq_song_database:
@@ -42,7 +62,8 @@ class sq_song_database:
         print(db_entry)
 
     def __init__(self, path="./database.json"):
-        """Create a new song database.
+        """
+        Create a new song database.
 
         Keyword argument:
         path -- the pathname a JSON database (defaults to './database.json')
@@ -55,7 +76,8 @@ class sq_song_database:
             self.songs.append(song)
 
     def pick_song(self):
-        """Pick a random song from the database.
+        """
+        Pick a random song from the database.
 
         Then remove it from the database and return it to the caller.
         """
@@ -126,8 +148,8 @@ class sq_song:
         except KeyError:
             return False
 
-        match_str = _make_match_candidate(thing)
-        candidate_str = _make_match_candidate(other)
+        match_str = sq_util.make_match_candidate(thing)
+        candidate_str = sq_util.make_match_candidate(other)
 
         return match_str.lower() == candidate_str.lower()
 
@@ -143,6 +165,7 @@ class sq_game_difficulty:
     }
 
     def __init__(self):
+        print("\nSelect your difficulty:\n")
         for diff in self.diff_dict:
             context = self.diff_dict[diff]["context"]
             line_str = " line" if context == 1 else " lines"
@@ -160,3 +183,89 @@ class sq_game_difficulty:
                 continue
             except EOFError:
                 sys.exit(0)
+
+
+class sq_game:
+    difficulty = None
+    db = None
+    scores = {
+        "artist": 5,
+        "title": 5
+    }
+    max_question_score = 0
+    max_count = 0
+    max_score = 0
+    current_score = 0
+
+    def __init__(self, filename="./database.json"):
+        self.db = sq_song_database(filename)
+        self.max_question_score = reduce(
+            (lambda x, y: x + y), self.scores.values())
+        print(f"database loaded: {str(len(self.db.songs))} songs present")
+
+    def get_max_count(self):
+        while True:
+            try:
+                self.max_count = int(
+                    input(f"How many? (max {len(self.db.songs)}) > "))
+                break
+            except ValueError:
+                self.max_count = len(self.db.songs)
+
+    def run(self):
+        count = 0
+        self.difficulty = sq_game_difficulty()
+        print(f"\"{self.difficulty.value.title()}\" difficulty chosen\n")
+        print("Scoring is as follows:")
+        for k, v in self.scores.items():
+            print(f"\t{v} points per correct {k}")
+        self.get_max_count()
+        self.max_score = self.max_count * self.max_question_score
+        while count < self.max_count:
+            this_question_score = self.do_question()
+            self.current_score += this_question_score
+            count += 1
+            what = "song" if self.max_count - count == 1 else "songs"
+            remains = "remains" if self.max_count - count == 1 else "remain"
+            print(
+                f"{this_question_score} points added to your score.  {self.max_count - count} {what} {remains}.")
+            print(
+                f"Current score: {str(self.current_score)}/{str(self.max_score)}\n")
+        print("Final score:", str(self.current_score) + "/" + str(self.max_score))
+
+    def do_question(self):
+        songinfo = {
+            "artist": {"guess": str(), "correct?": False},
+            "title":  {"guess": str(), "correct?": False}
+        }
+
+        this_question_score = 0
+
+        try:
+            song = self.db.pick_song()
+            for line in song.pick_lines(self.difficulty):
+                print(line)
+        except ValueError:
+            return None
+
+        try:
+            for key in songinfo.keys():
+                songinfo[key]["guess"] = input(f"Guess the {key}! > ")
+        except EOFError:
+            return None
+
+        for key in songinfo.keys():
+            if song.loose_match(songinfo[key]["guess"], key) == True:
+                songinfo[key]["correct?"] = True
+                this_question_score += self.scores[key]
+
+        if this_question_score == self.max_question_score:
+            print(random.choice(sq_util.feedbacks["perfect"]))
+        else:
+            for key in songinfo.keys():
+                if songinfo[key]["correct?"] == True:
+                    print(random.choice(sq_util.feedbacks[key]))
+                elif songinfo[key]["correct?"] == False:
+                    print(f"The correct {key} is {song.info[key]}.")
+
+        return this_question_score
